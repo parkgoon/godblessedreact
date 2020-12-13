80d82d5c47189c55760cfd2b3e106463fb95c205
junggoonpark <ohdury@gmail.com>
2020-12-13 23:42:41 +0900

    debug 완료

--- WordRelay.jsx
+++ WordRelay.jsx
@@ -1,10 +1,10 @@
-const React=require('react');
-const { Componet } = React;
+const React = require('react');
+const { Component } = React;
 
 
 class WordRelay extends Component {
     state = {
-        text: 'test',
+        text: '끝말잇기 : webpack.config.js 통하여 babel rule 적용 ; wordrelay.jsx 통하려 렌더링 ; package.json 에 node 의존성 생성 ; app.js 를 참조하여 index.html 표현',
     };
 
     render(){
