5d784b610be89de6c60149feb958928ce8bb036c
junggoonpark <ohdury@gmail.com>
2020-12-19 23:04:19 +0900

    끝말잇기 #2 구구단을 응용하여 상태값과 로직을 업데이트

--- WordRelay.jsx
+++ WordRelay.jsx
@@ -4,11 +4,48 @@ const { Component } = React;
 
 class WordRelay extends Component {
     state = {
-        text: '끝말잇기 : webpack.config.js 통하여 babel rule 적용 ; wordrelay.jsx 통하려 렌더링 ; package.json 에 node 의존성 생성 ; app.js 를 참조하여 index.html 표현',
+        word: '삼손',
+        value: '',
+        result: '',
     };
 
+    onSubmitForm=(e)=>{
+        e.preventDefault();
+        if(this.state.word[this.state.word.length -1] === this.state.value[0]){
+            this.setState({
+                result: '어쭈',
+                word: this.state.value,
+                value: '',
+            });
+            this.input.focus();
+        }else {
+            this.setState({
+                result: '머래',
+                value: '',
+            })
+            this.input.focus();
+        }
+    };
+
+    onChangeInput =(e)=>{
+        this.setState({value: e.target.value});
+    };
+
+    onRefInput=(c)=>{
+        this.input =c;
+    }
+
     render(){
-        return <h1>{this.state.text}</h1>;
+        return (
+            <>
+                <div>{this.state.word} 쿵쿵따</div>
+                <form onSubmit={this.onSubmitForm}>
+                    <input ref={this.onRefInput} value={this.state.value} onChange={this.onChangeInput} />
+                    <button>입력!</button>
+                </form>
+                <div>{this.state.result}</div>
+            </>
+        );
     }
 }
 
